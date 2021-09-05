8f64151ca03002b4a548b436f2a7158cbc094e05
YmendezM <ysmendezm@gmail.com>
2021-09-05 09:30:50 -0300

    Import component Vue

---
+++ src/components/Count.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <button v-on:click="increments">++</button>
+        <span>Valor de contador {{ count }}</span>
+    </div>
+</template>
+<script>
+export default {
+    name:"Count",
+    data(){
+        return{
+            count: 0
+        }
+    },
+    methods: {
+        increments: function(){
+            this.count++
+        }
+    },
+}
+</script>
